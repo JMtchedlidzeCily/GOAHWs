d224315f59dcd237eab2f6ac3e250c8d940ff0e8
JMtchedlidzeCily <Mtchedlidzejarji@gmail.com>
2024-05-18 21:28:14 +0400

    commit

---
+++ mini leaderis davaleba/hw2.py
@@ -0,0 +1,6 @@
+deda = input("dedatqvenis asaki: ")
+mama = input("mamatqvenis asaki: ")
+da = input("tqveni dis asaki: ")
+print("dedatqveni iqneba ", int(deda) + 2 , " wlis ori wlis shemdeg")
+print("mamatqveni iqneba ", int(mama) + 2 , " wlis ori wlis shemdeg")
+print("tqveni da iqneba ", int(da) + 2 , " wlis ori wlis shemdeg")
