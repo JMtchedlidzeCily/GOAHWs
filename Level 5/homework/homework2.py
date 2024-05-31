c1bbec585732666bdec3edde5af548b82838f11f
JMtchedlidzeCily <Mtchedlidzejarji@gmail.com>
2024-05-31 20:32:50 +0400

    commit

---
+++ Level 5/homework/homework2.py
@@ -0,0 +1,20 @@
+str1= "GIA"
+int1 = 16
+fstr1 = f" I have a friend named ", str1, " he is ", int1, " years old"
+print(fstr1)
+str2= "fluffy"
+int2 = 3
+fstr2 = f" I have a dog named ", str1, " he is ", int1, " dog years old"
+print(fstr1)
+str1= "sololearn?"
+int1 = 5
+fstr1 = f" have you heard about this new site ", str1, " all of the cool kids use it, I already had ", int1, " lessons!"
+print(fstr1)
+str1= "pubg"
+int1 = 7
+fstr1 = f"Imagine playing ", str1, " such a game for ", int1, "y/o's "
+print(fstr1)
+str1= "python"
+int1 = 33
+fstr1 = f"fun fact: ", str1, " is ", int1, " years old!"
+print(fstr1)
