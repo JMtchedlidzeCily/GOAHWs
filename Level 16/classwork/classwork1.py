8a321162e7901738a0639b2d06e511da5b847634
JMtchedlidzeCily <Mtchedlidzejarji@gmail.com>
2024-07-06 08:26:01 -0700

    commit

---
+++ Level 16/classwork/classwork1.py
@@ -0,0 +1,3 @@
+list1 = [1, True, 2, "chad", 3, False, 4, "sigma", 5, "mogger"]
+#        0    1   2     3    4    5    6    7      8     9
+print(list1)
---
+++ Level 16/classwork/classwork2.py
@@ -0,0 +1,4 @@
+list1 = [1, True, 2, "chad", 3, False, 4, "sigma", 5, "mogger"]
+print(list1[:3])
+print(list1[-3:])
+print(list1[3:7])
