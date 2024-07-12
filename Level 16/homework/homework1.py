6cc761080cbbe9c94431c9214c6a42e2b0e0273f
JMtchedlidzeCily <Mtchedlidzejarji@gmail.com>
2024-07-12 04:40:04 -0700

    commit

---
+++ Level 16/homework/homework1.py
@@ -0,0 +1,4 @@
+integer_list = [1, 2, 3, 4, 5]
+
+for i in range(len(integer_list)):
+    print(integer_list[i] * 2)
---
+++ Level 16/homework/homework2.py
@@ -0,0 +1,4 @@
+string_list = ["data", "luka"]
+
+for i in range(len(string_list)):
+    print("#" + string_list[i])
