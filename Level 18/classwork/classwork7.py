cb336f7f19bf4962fa10f6f6b2d27c6f2ec3abc0
JMtchedlidzeCily <Mtchedlidzejarji@gmail.com>
2024-07-19 02:57:52 -0700

    commit

---
+++ Level 18/classwork/classwork7.py
@@ -0,0 +1,7 @@
+def print_list_elements(my_list):
+    for element in my_list:
+        print(element)
+
+# Example usage:
+sample_list = ["apple", "banana", "cherry"]
+print_list_elements(sample_list)
